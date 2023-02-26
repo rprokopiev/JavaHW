336a2bcb02c85ee5133360a4ae8ad107bb2c82e8
Renat Prokopiev <prokop4891@gmail.com>
2023-02-26 18:46:18 +0300

    Del Honest numbers from list

---
+++ 009_DeleteHonestNumbersFromList/DeleteHonestNumbersFromList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/** Пусть дан произвольный список целых чисел. Удалить из него чётные числа. */
+
+public class DeleteHonestNumbersFromList {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = RandomIntListGenerator(8, 1, 15);
+        System.out.println(list);
+        list = RemoveHonestNumbersFromList(list);
+        System.out.println(list);
+    }
+
+    // создание листа случайных чисел
+    private static ArrayList<Integer> RandomIntListGenerator(Integer n, Integer min, Integer max) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            Random r = new Random();
+            list.add(r.nextInt(min, max));
+        }
+        return list;
+    }
+    
+    private static ArrayList<Integer> RemoveHonestNumbersFromList(ArrayList<Integer> list){
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i)%2 == 0){
+                list.remove(i);
+                i--;
+            }
+        }
+        return list;
+    }
+}
